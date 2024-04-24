576f6b173a7fe0712a59f35f05cc3812db8968e9
ilyapc <ilya.komendantov@list.ru>
2024-04-24 15:51:43 +0300

    Initial commit

---
+++ app/src/main/java/ru/ikom/jetwords/MainViewModel.kt
@@ -0,0 +1,44 @@
+package ru.ikom.jetwords
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.flow
+
+class MainViewModel : ViewModel() {
+
+    val words = flow {
+        emit(
+            listOf(
+                WordUi(
+                    0,
+                    listOf(
+                        LettersMode.Basis("справ", "к"),
+                        LettersMode.Ending("а")
+                    )
+                ),
+                WordUi(
+                    1,
+                    listOf(
+                        LettersMode.Basis("розетк"),
+                        LettersMode.Ending("а")
+                    )
+                ),
+                WordUi(
+                    2,
+                    listOf(
+                        LettersMode.Basis("клави", "ат ур"),
+                        LettersMode.Ending("а")
+                    )
+                )
+            )
+        )
+    }
+
+}
+
+sealed interface LettersMode {
+
+    data class Basis(val root: String, val suffix: String? = null) : LettersMode
+    data class Ending(val letters: String) : LettersMode
+}
+
+data class WordUi(val id: Int, val letters: List<LettersMode>)
